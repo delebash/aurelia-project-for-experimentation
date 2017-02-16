678af1d6fe1aa72b7e16df81f944ac0af54e4db8
Daniel Elebash <danelebash@hotmail.com>
2017-02-16 15:15:36 -0500

    comment

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import {inject} from 'aurelia-framework';
 import routes from './config/config.routes'
 
+// Load routes from /config/config.routes
 export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
